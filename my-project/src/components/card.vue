2e2eb2c879b7e242937db70afc529e8f46718add
hwt1117 <847831004@qq.com>
2019-08-02 10:59:05 +0800

    工作帮

---
+++ my-project/src/components/card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+      <i class="icon-search"></i>
+      <input
+        type="text"
+        v-model="msg"
+        placeholder="输入感兴趣的关键词或发布者昵称"
+        placeholder-style="color: #858585;"
+        class="inp1"
+      />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>  
+
+.icon-search {
+  background: url(../../static/images/search1.png) no-repeat;
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  top: 5px;
+  left: 40px;
+}
+.inp1 {
+  border-radius: 50px;
+  padding-left: 60px;
+  background-color: #9999991e;
+  width: 270px;
+  height: 30px;
+  margin: 0 auto;
+}
+</style>
---
+++ my-project/src/pages/zufang/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div style="height:200px;backgroundcolor:black">
+        <input type="text" v-model="msg">
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return{
+            msg:""
+        }
+    }
+}
+</script>
+
+
